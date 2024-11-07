bfddc924fda40f5ca312e347e5c2eec587fbb4f1
wmsxwdsh <maxnwaados@icloud.com>
2024-11-07 22:57:07 +0800

    Create sublime-settings.py

---
+++ sublime-settings.py
@@ -0,0 +1,27 @@
+// Settings in here override those in "Default/Preferences.sublime-settings",
+// and are overridden in turn by syntax-specific settings.
+{
+  // 检查更新
+  "update_check": false,
+  // 显示空格
+  // "draw_white_space": "all",
+
+  // 不进入 VIM模式
+  "ignored_packages":
+  [
+    "Vintage",
+  ],
+  "open_files_in_new_window": false,
+  "save_on_focus_lost": true,
+
+  "remember_open_files": true,
+  "hot_exit": true,
+  "default_encoding": "UTF-8",
+
+  "show_encoding": true,
+  "show_line_endings": true,
+
+  "font_size": 17,
+  "theme": "Default Dark.sublime-theme",
+  "index_files": true,
+}
